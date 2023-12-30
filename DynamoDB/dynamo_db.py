b04c1cd3288a8e3bb234101ca25e6baf380fea28
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-12-30 19:42:00 +0530

    Create dynamo_db.py

---
+++ DynamoDB/dynamo_db.py
@@ -0,0 +1,40 @@
+import boto3
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='heydevops',
+    KeySchema=[
+        {
+            'AttributeName': 'username',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'last_name',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'username',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'last_name',
+            'AttributeType': 'S'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+# Wait until the table exists.
+table.wait_until_exists()
+
+
+# Print out some data about the table.
+print(table.item_count)
