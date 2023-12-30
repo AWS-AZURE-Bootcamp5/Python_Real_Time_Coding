b35e2b499144af92b1a6fc8a06b22dce15c55ed5
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-12-30 19:41:25 +0530

    Create upload_s3.py

---
+++ S3/upload_s3.py
@@ -0,0 +1,20 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+
+def upload_file(file_name, bucket, object_name=None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+upload_file("/Users/praveensingampalli/Documents/MIT.pptx","singamheydevops")
