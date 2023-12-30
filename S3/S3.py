aa3039b57da35484f903463b1a35307bf61fc72d
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-12-30 19:40:59 +0530

    Create S3.py

---
+++ S3/S3.py
@@ -0,0 +1,22 @@
+import boto3
+
+import logging
+from botocore.exceptions import ClientError
+
+def create_bucket(bucket_name, region=None):
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+create_bucket("singamheydevops","us-west-1")
