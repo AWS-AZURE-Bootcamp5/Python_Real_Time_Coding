dddbe3f41398848e129bd7314d67ff644d0c8c6c
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-12-30 19:43:16 +0530

    Create PutData_Db.py

---
+++ DynamoDB/PutData_Db.py
@@ -0,0 +1,21 @@
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('heydevops')
+print(table.creation_date_time)
+table.put_item(
+   Item={
+        'username': 'janedoe',
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'age': 25,
+        'account_type': 'standard_user',
+    }
+)
+response = table.get_item(
+    Key={
+        'username': 'janedoe',
+        'last_name': 'Doe'
+    }
+)
+item = response['Item']
+print(item)
