fa9c24cba27eb9ce9c0f22e998ac34eaf2611c1b
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-12-30 19:45:37 +0530

    Create Hit_Endpoint.py

---
+++ Hit_Endpoint.py
@@ -0,0 +1,20 @@
+#pip install requests
+#pip install beautifulsoup4
+import requests
+from bs4 import BeautifulSoup
+url = 'https://archive.ics.uci.edu/ml/datasets.php'
+
+response = requests.get(url)
+content = response.content # we get all the content from the website
+soup = BeautifulSoup(content, 'html.parser') # beautiful soup will give a chance to parse
+print(soup.title) # <title>UCI Machine Learning Repository: Data Sets</title>
+print(soup.title.get_text()) # UCI Machine Learning Repository: Data Sets
+print(soup.body) # gives the whole page on the website
+print(response.status_code)
+
+tables = soup.find_all('table', {'cellpadding':'3'})
+# We are targeting the table with cellpadding attribute with the value of 3
+# We can select using id, class or HTML tag , for more information check the beautifulsoup doc
+table = tables[0] # the result is a list, we are taking out data from it
+for td in table.find('tr').find_all('td'):
+    print(td.text)
